7318b6f97a43f8e34f40a7a6e3cac4f242257254
Kyle Jessup <badthing@gmail.com>
2016-07-06 16:13:27 -0400

    Moved to own repository

---
+++ Tests/LinuxMain.swift
@@ -0,0 +1,6 @@
+import XCTest
+@testable import PerfectMustacheTestSuite
+
+XCTMain([
+     testCase(PerfectMustacheTests.allTests),
+])
---
+++ Tests/PerfectMustache/PerfectMustacheTests.swift
@@ -0,0 +1,15 @@
+import XCTest
+@testable import PerfectMustache
+
+class PerfectMustacheTests: XCTestCase {
+    func testExample() {
+		
+    }
+
+
+    static var allTests : [(String, (PerfectMustacheTests) -> () throws -> Void)] {
+        return [
+            ("testExample", testExample),
+        ]
+    }
+}
